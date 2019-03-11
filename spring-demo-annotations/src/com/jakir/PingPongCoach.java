af652a724c36d23d0121943539ec6ab6292374e6
jakir-cokreates <jakir@cokreates.com>
2019-03-11 10:34:19 +0600

    fourth assignment complete

---
+++ spring-demo-annotations/src/com/jakir/PingPongCoach.java
@@ -0,0 +1,18 @@
+package com.jakir;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PingPongCoach implements Coach {
+
+	// define a default constructor
+	public PingPongCoach() {
+		System.out.println(">> PingPongCoach: inside default constructor");
+	}
+		
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your pingpong drop shot";
+	}
+
+}
